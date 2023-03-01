415423f5bcbf3795dfb5ae878d176db2e0658d16
kevinscaria <kscaria@cg20-1.agave.rc.asu.edu>
2023-03-01 13:27:05 -0700

    Added scripts

---
+++ Scripts/ate_train.sh
@@ -0,0 +1,5 @@
+python ../run_model.py -mode train -model_checkpoint allenai/tk-instruct-base-def-pos \
+-experiment_name check_dummy -task ate -output_dir ../Models \
+-id_tr_data_path ../Dataset/example_data.csv \
+-id_te_data_path ../Dataset/example_data.csv \
+-per_device_train_batch_size 16 -per_device_eval_batch_size 16 -num_train_epochs 4

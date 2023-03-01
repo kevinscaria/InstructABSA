f93b754712c6147beb9f178582250f397211e096
kevinscaria <kscaria@cg20-1.agave.rc.asu.edu>
2023-03-01 15:25:39 -0700

    Added all scripts and sample Output folder

---
+++ Scripts/atsc_eval.sh
@@ -0,0 +1,5 @@
+python ../run_model.py -mode eval -model_checkpoint ../Models/atsc/allenai/tk-instruct-base-def-pos-check_dummy \
+-experiment_name atsc_check -task atsc -output_path ../Output \
+-id_tr_data_path ../Dataset/example_data.csv \
+-id_te_data_path ../Dataset/example_data.csv \
+-per_device_train_batch_size 16 -per_device_eval_batch_size 16 -num_train_epochs 4

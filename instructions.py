81693594b7f3991396bba423ab0b022519a82804
Kevin Scaria <scariakevin1@gmail.com>
2023-07-04 10:30:47 -0400

    Added new instruction set for opinion based tasks

--- instructions.py
+++ instructions.py
@@ -2,9 +2,15 @@ class InstructionsHandler:
     def __init__(self):
         self.ate = {}
         self.atsc = {}
-        self.joint = {}
+        self.aspe = {}
+        self.aooe = {}
+        self.aope = {}
+        self.aoste = {}
 
     def load_instruction_set1(self, ):
+
+        ################################# ATE #################################
+
         self.ate['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) which have an associated opinion that are extracted from the input text. In cases where there are no aspects the output should be noaspectterm.
         Positive example 1-
         input: I charge it at night and skip taking the cord with me because of the good battery life.
@@ -26,6 +32,8 @@ class InstructionsHandler:
         input: """
         self.ate['delim_instruct'] = ''
         self.ate['eos_instruct'] = ' \noutput:'
+
+        ################################# ATSC #################################
 
         self.atsc['bos_instruct1'] = """Definition: The output will be 'positive' if the aspect identified in the sentence contains a positive sentiment. If the sentiment of the identified aspect in the input is negative the answer will be 'negative'. 
         Otherwise, the output should be 'neutral'. For aspects which are classified as noaspectterm, the sentiment is none.
@@ -51,7 +59,9 @@ class InstructionsHandler:
         self.atsc['delim_instruct'] = ' The aspect is '
         self.atsc['eos_instruct'] = '.\noutput:'
 
-        self.joint['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        ################################# ASPE #################################
+
+        self.aspe['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
         Positive example 1-
         input: I charge it at night and skip taking the cord with me because of the good battery life.
         output: battery life:positive, 
@@ -61,7 +71,7 @@ class InstructionsHandler:
         Now complete the following example-
         input: """
         
-        self.joint['bos_instruct2'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        self.aspe['bos_instruct2'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
         Positive example 1-
         input: With the great variety on the menu , I eat here often and never get bored.
         output: menu:positive
@@ -70,10 +80,84 @@ class InstructionsHandler:
         output: food:positive, menu:positive, service:positive, setting:positive
         Now complete the following example-
         input: """
-        self.joint['delim_instruct'] = ''
-        self.joint['eos_instruct'] = ' \noutput:'
+        self.aspe['delim_instruct'] = ''
+        self.aspe['eos_instruct'] = ' \noutput:'
+
+        ################################# AOOE - CURRENT #################################
+
+        self.aooe['bos_instruct1'] = """Definition: The output will be the opinion/describing word of the aspect terms in the sentence. In cases where there are no aspects the output should be none.
+        Positive example 1-
+        input: I charge it at night and skip taking the cord with me because of the good battery life . The aspect is battery life.
+        output: good
+        Positive example 2-
+        input: it is of high quality , has a killer GUI , is extremely stable , is highly expandable , is bundled with lots of very good applications , is easy to use , and is absolutely gorgeous. The aspect is GUI.
+        output: killer
+        Now complete the following example-
+        input: """
+        
+        self.aooe['bos_instruct2'] = """Definition: The output will be the opinion/describing word for the aspect term in the sentence. In cases where there are no aspects the output should be none.
+        Positive example 1-
+        input: Faan 's got a great concept but a little rough on the delivery . The aspect term is delivery.
+        output: rough
+        Positive example 2- 
+        input: At the end you 're left with a mild broth with noodles that you can slurp out of a cup . The aspect term is broth with noodles.
+        output: mild
+        Now complete the following example-
+        input: """
+        self.aooe['delim_instruct'] = ' The aspect is '
+        self.aooe['eos_instruct'] = '.\noutput:'
+
+        ################################# AOPE #################################
+
+        self.aope['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        Positive example 1-
+        input: I charge it at night and skip taking the cord with me because of the good battery life.
+        output: battery life:positive, 
+        Positive example 2-
+        input: it is of high quality , has a killer GUI , is extremely stable , is highly expandable , is bundled with lots of very good applications , is easy to use , and is absolutely gorgeous.
+        output: quality:high, GUI:killer, 
+        Now complete the following example-
+        input: """
+        
+        self.aope['bos_instruct2'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        Positive example 1-
+        input: With the great variety on the menu , I eat here often and never get bored.
+        output: menu:positive
+        Positive example 2- 
+        input: Great food, good size menu, great service and an unpretensious setting.
+        output: food:positive, menu:positive, service:positive, setting:positive
+        Now complete the following example-
+        input: """
+        self.aope['delim_instruct'] = ''
+        self.aope['eos_instruct'] = ' \noutput:'
+
+        self.aoste['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        Positive example 1-
+        input: I charge it at night and skip taking the cord with me because of the good battery life.
+        output: battery life:positive, 
+        Positive example 2-
+        input: I even got my teenage son one, because of the features that it offers, like, iChat, Photobooth, garage band and more!.
+        output: features:positive, iChat:positive, Photobooth:positive, garage band:positive
+        Now complete the following example-
+        input: """
+        
+        self.aoste['bos_instruct2'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        Positive example 1-
+        input: With the great variety on the menu , I eat here often and never get bored.
+        output: menu:positive
+        Positive example 2- 
+        input: Great food, good size menu, great service and an unpretensious setting.
+        output: food:positive, menu:positive, service:positive, setting:positive
+        Now complete the following example-
+        input: """
+        self.aoste['delim_instruct'] = ''
+        self.aoste['eos_instruct'] = ' \noutput:'
+
 
     def load_instruction_set2(self, ):
+
+        ################################# ATE #################################
+
         self.ate['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) which have an associated opinion that are extracted from the input text. In cases where there are no aspects the output should be noaspectterm.
         Positive example 1-
         input: I charge it at night and skip taking the cord with me because of the good battery life.
@@ -119,6 +203,8 @@ class InstructionsHandler:
         input: """
         self.ate['delim_instruct'] = ''
         self.ate['eos_instruct'] = ' \noutput:'
+
+        ################################# ATSC #################################
 
         self.atsc['bos_instruct1'] = """Definition: The output will be 'positive' if the aspect identified in the sentence contains a positive sentiment. If the sentiment of the identified aspect in the input is negative the answer will be 'negative'. 
         Otherwise, the output should be 'neutral'. For aspects which are classified as noaspectterm, the sentiment is none.
@@ -168,7 +254,9 @@ class InstructionsHandler:
         self.atsc['delim_instruct'] = ' The aspect is '
         self.atsc['eos_instruct'] = '.\noutput:'
 
-        self.joint['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        ################################# ASPE #################################
+
+        self.aspe['bos_instruct1'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
         Positive example 1-
         input: I charge it at night and skip taking the cord with me because of the good battery life.
         output: battery life:positive, 
@@ -190,7 +278,7 @@ class InstructionsHandler:
         Now complete the following example-
         input: """
         
-        self.joint['bos_instruct2'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
+        self.aspe['bos_instruct2'] = """Definition: The output will be the aspects (both implicit and explicit) and the aspects sentiment polarity. In cases where there are no aspects the output should be noaspectterm:none.
         Positive example 1-
         input: With the great variety on the menu , I eat here often and never get bored.
         output: menu:positive
@@ -211,5 +299,53 @@ class InstructionsHandler:
         output: glass of wine:neutral
         Now complete the following example-
         input: """
-        self.joint['delim_instruct'] = ''
-        self.joint['eos_instruct'] = ' \noutput:'
+        self.aspe['delim_instruct'] = ''
+        self.aspe['eos_instruct'] = ' \noutput:'
+
+        ################################# AOOE - Current #################################
+
+        self.aooe['bos_instruct1'] = """Definition: The output will be the opinion/describing word of the aspect terms in the sentence. In cases where there are no aspects the output should be none.
+        Positive example 1-
+        input: I charge it at night and skip taking the cord with me because of the good battery life . The aspect is battery life.
+        output: good
+        Positive example 2-
+        input: it is of high quality , has a killer GUI , is extremely stable , is highly expandable , is bundled with lots of very good applications , is easy to use , and is absolutely gorgeous. The aspect is GUI.
+        output: killer
+        Negative example 1-
+        input: One night I turned the freaking thing off after using it , the next day I turn it on , no GUI , screen all dark , power light steady , hard drive light steady and not flashing as it usually does . The aspect is GUI.
+        output: no
+        Negative example 2-
+        input: I can barely use any usb devices because they will not stay connected properly . The aspect is usb devices.
+        output: not stay connected properly
+        Neutral example 1-
+        input: However , the multi-touch gestures and large tracking area make having an external mouse unnecessary ( unless you 're gaming ) . The aspect is external mouse.
+        output: unnecessary
+        Neutral example 2-
+        input: I wanted to purchase the extended warranty and they refused , because they knew it was trouble . The aspect is extended warranty.
+        output: refused
+        Now complete the following example-
+        input: """
+
+        self.aooe['bos_instruct2'] = """Definition: The output will be the opinion/describing word of the aspect terms in the sentence. In cases where there are no aspects the output should be none.
+        Positive example 1-
+        input: I charge it at night and skip taking the cord with me because of the good battery life . The aspect is battery life.
+        output: good
+        Positive example 2-
+        input: it is of high quality , has a killer GUI , is extremely stable , is highly expandable , is bundled with lots of very good applications , is easy to use , and is absolutely gorgeous. The aspect is GUI.
+        output: killer
+        Negative example 1-
+        input: The menu is very limited - i think we counted 4 or 5 entrees . The aspect is menu.
+        output: limited
+        Negative example 2-
+        input: The strong scents coming from the left and right of me negatively affected my taste buds . The aspect is scents.
+        output: strong
+        Neutral example 1-
+        input: What came to our table was burned beyond recognition and stringy . The aspect is battery table.
+        output: burned
+        Neutral example 2-
+        input: But , nothing stands out about the cooking . The aspect is cooking.
+        output: nothing stands out
+        Now complete the following example-
+        input: """
+        self.aooe['delim_instruct'] = ' The aspect is '
+        self.aooe['eos_instruct'] = '.\noutput:'
